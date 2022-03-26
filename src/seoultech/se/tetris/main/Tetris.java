ff1e2add8a40c510da3effcf50167eb1b39a4000
HjwSoll <hello.git.eric25525@naver.com>
2022-03-26 11:37:32 +0900

    시작 메뉴 구현, 디자인 필요

--- src/seoultech/se/tetris/main/Tetris.java
+++ src/seoultech/se/tetris/main/Tetris.java
@@ -5,8 +5,6 @@ import seoultech.se.tetris.component.Board;
 public class Tetris {
 
 	public static void main(String[] args) {
-		Board main = new Board();
-		main.setSize(400, 500);
-		main.setVisible(true);
+		new startMenu();
 	}
 }
