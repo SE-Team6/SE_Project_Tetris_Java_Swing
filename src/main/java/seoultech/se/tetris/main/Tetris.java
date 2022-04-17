5ffe42a0b753cbb77b1204afc7fceb5aca67296f
ha4219 <ha4219@naver.com>
2022-04-17 19:18:38 +0900

    item type 3 and 4 added

--- src/main/java/seoultech/se/tetris/main/Tetris.java
+++ src/main/java/seoultech/se/tetris/main/Tetris.java
@@ -1,19 +1,22 @@
 package seoultech.se.tetris.main;
 
+import seoultech.se.tetris.component.board.Board;
+import seoultech.se.tetris.component.board.ItemBoard;
+import seoultech.se.tetris.component.board.NormalBoard;
 import seoultech.se.tetris.menu.StartMenu;
 
 public class Tetris {
 
 	public static void main(String[] args) {
-//		Board main = new ItemBoard();
-////		Board main = new NormalBoard();
+		Board main = new ItemBoard();
+//		Board main = new NormalBoard();
+
+		main.setSize(600, 800);
+		main.setLocation(0, 0);
 //
-//		main.setSize(400, 450);
-//		main.setLocation(0, 0);
-////
-////
-//		main.setVisible(true);
-		System.out.println(System.getProperty("user.dir"));
-		StartMenu main = new StartMenu();
+//
+		main.setVisible(true);
+//		System.out.println(System.getProperty("user.dir"));
+//		StartMenu main = new StartMenu();
 	}
 }
