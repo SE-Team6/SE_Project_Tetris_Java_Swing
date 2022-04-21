59439cde1f007fc7dbc708a076bd9f0a11d5de54
khyogeun_@naver.com <khyogeun_@naver.com>
2022-04-22 05:11:49 +0900

    설정파일 수정

--- src/main/java/seoultech/se/tetris/component/JSONLoader.java
+++ src/main/java/seoultech/se/tetris/component/JSONLoader.java
@@ -3,48 +3,55 @@ package seoultech.se.tetris.component;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class JSONLoader {
-    final static String SETTINGS_FILEPATH = JSONWriter.class.getResource("/configs/settings.json").toString();
-    final static String NORMAL_SCORE_FILEPATH = JSONWriter.class.getResource("/configs/normal_score.json").toString();
-    final static String ITEM_SCORE_FILEPATH = JSONWriter.class.getResource("/configs/item_score.json").toString();
+    static String SETTINGS_FILEPATH = "./config/settings.json";
+    static String NORMAL_SCORE_FILEPATH = "./config/normal_score.json";
+    static String ITEM_SCORE_FILEPATH = "./config/item_score.json";
 
     static JSONParser parser = new JSONParser();
 
     JSONLoader(){}
 
+    public static String jarPathToFile(String path) {
+        String s = null;
+        try{
+            InputStream stream = JSONLoader.class.getResourceAsStream(path);
+            byte[] bytes = stream.readAllBytes();
+            s = new String(bytes, StandardCharsets.UTF_8);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+
     public static Object getJSONObject(String type, String key){
         JSONObject obj = new JSONObject();
         try {
-            switch (type){
+            switch (type) {
                 case "settings":
                     FileReader settingFile = new FileReader(SETTINGS_FILEPATH);
                     obj = (JSONObject) parser.parse(settingFile);
                     settingFile.close();
                     break;
                 case "normal":
-                    FileReader normalScoreFile = new FileReader(NORMAL_SCORE_FILEPATH);
-                    obj = (JSONObject) parser.parse(normalScoreFile);
-                    normalScoreFile.close();
+                    FileReader normalFile = new FileReader(NORMAL_SCORE_FILEPATH);
+                    obj = (JSONObject) parser.parse(normalFile);
+                    normalFile.close();
                     break;
                 case "item":
-                    FileReader itemScoreFile = new FileReader(ITEM_SCORE_FILEPATH);
-                    obj = (JSONObject) parser.parse(itemScoreFile);
-                    itemScoreFile.close();
+                    FileReader itemFile = new FileReader(ITEM_SCORE_FILEPATH);
+                    obj = (JSONObject) parser.parse(itemFile);
+                    itemFile.close();
                     break;
             }
-        } catch (FileNotFoundException e){
+        }catch (Exception e){
             e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
-        } catch (ParseException e){
-            System.out.println("Score Board is Empty!");
         }
         return obj.get(key);
     }
--- src/main/java/seoultech/se/tetris/component/JSONWriter.java
+++ src/main/java/seoultech/se/tetris/component/JSONWriter.java
@@ -4,17 +4,15 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 import static seoultech.se.tetris.component.JSONLoader.*;
 
 public class JSONWriter {
-    final static String SETTINGS_FILEPATH = JSONWriter.class.getResource("/configs/settings.json").toString();
-    final static String NORMAL_SCORE_FILEPATH = JSONWriter.class.getResource("/configs/normal_score.json").toString();
-    final static String ITEM_SCORE_FILEPATH = JSONWriter.class.getResource("/configs/item_score.json").toString();
+    final static String SETTINGS_FILEPATH = "./config/settings.json";
+    final static String NORMAL_SCORE_FILEPATH = "./config/normal_score.json";
+    final static String ITEM_SCORE_FILEPATH = "./config/item_score.json";
 
     JSONWriter(){}
 
@@ -117,14 +115,14 @@ public class JSONWriter {
         result.put("scoreBoard", arr2);
 
         if(mode.equals("normal")){
-            try(FileWriter file = new FileWriter(NORMAL_SCORE_FILEPATH.split(":")[2])){
+            try(FileWriter file = new FileWriter(NORMAL_SCORE_FILEPATH)){
                 file.write(result.toJSONString());
                 file.flush();
             } catch (IOException e){
                 e.printStackTrace();
             }
         }else if(mode.equals("item")){
-            try(FileWriter file = new FileWriter(ITEM_SCORE_FILEPATH.split(":")[2])){
+            try(FileWriter file = new FileWriter(ITEM_SCORE_FILEPATH)){
                 file.write(result.toJSONString());
                 file.flush();
             } catch (IOException e){
@@ -138,13 +136,13 @@ public class JSONWriter {
     public static void resetScore(String mode) {
         if (mode.equals("normal")) {
             try {
-                new FileOutputStream(NORMAL_SCORE_FILEPATH.split(":")[2]).close();
+                new FileOutputStream(NORMAL_SCORE_FILEPATH).close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else if (mode.equals("item")) {
             try {
-                new FileOutputStream(ITEM_SCORE_FILEPATH.split(":")[2]).close();
+                new FileOutputStream(ITEM_SCORE_FILEPATH).close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -178,7 +176,8 @@ public class JSONWriter {
         jsonList.put("colorBlindMode", colorBlindModeObj);
         jsonList.put("scoreBoardPage",scoreBoardPageObj);
 
-        try(FileWriter file = new FileWriter(SETTINGS_FILEPATH.split(":")[2])){
+
+        try(FileWriter file = new FileWriter(SETTINGS_FILEPATH)){
             file.write(jsonList.toJSONString());
             file.flush();
         } catch (IOException e){
